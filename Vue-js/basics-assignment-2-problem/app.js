8f34b5d1373b8e55544c746252229244739d9d50
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-09-07 11:08:19 +0100

    Added watch property.

--- Vue-js/basics-assignment-2-problem/app.js
+++ Vue-js/basics-assignment-2-problem/app.js
@@ -1,8 +1,16 @@
 Vue.createApp({
   data(){
     return {
+      counter: 0,
       userInput: '',
       confirmedName: ''
+    }
+  },
+  // watch for counter value and executes when condition is met
+  watch: {
+    counter(value){
+      if(value > 50)
+        this.counter = 0;
     }
   },
   methods: {
@@ -19,7 +27,14 @@ Vue.createApp({
     },
     deleteUserInput(){
       this.userInput = ''
+    },
+    add(number){
+      this.counter = this.counter + number
+    },
+    subtract(number){
+      this.counter = this.counter - number
     }
+    
 
   }
 }).mount('#assignment')

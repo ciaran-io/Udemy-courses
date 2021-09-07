8ba0a286130f9e5ad8141bf8f60058e942c55ca0
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-09-07 18:22:50 +0100

    Added list conditions

--- Vue-js/lists-cond-01-starting-setup/app.js
+++ Vue-js/lists-cond-01-starting-setup/app.js
@@ -1,7 +1,17 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { goals: [], input: '' };
   },
+  methods: {
+    addGoal: function(){
+      this.goals.push(this.input)
+      this.input = ''
+      console.log(this.goals)
+    }
+  },
+  // watch:{
+  //   input
+  // }
 });
 
 app.mount('#user-goals');

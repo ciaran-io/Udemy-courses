dd2d280f22dc33eabb7611408f6102e8a10fdc33
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-04-25 19:41:02 +0100

    feat: add mongoose connection

--- complete-node-bootcamp/4-natours/starter/server.js
+++ complete-node-bootcamp/4-natours/starter/server.js
@@ -1,9 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// Server start
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful'));
+
 const port = process.env.PORT || 8000;
 
 app.listen(port);

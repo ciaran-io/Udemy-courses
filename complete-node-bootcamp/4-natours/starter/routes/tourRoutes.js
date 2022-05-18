e5df7500d6fcc583650fbc028043841172cb990b
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-05-18 14:22:15 +0100

    feat: add new routes and authController

--- complete-node-bootcamp/4-natours/starter/routes/tourRoutes.js
+++ complete-node-bootcamp/4-natours/starter/routes/tourRoutes.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 // router.param('id', tourController.checkParamId);
+
+router
+  .route('/top-5-cheap-tours')
+  .get(tourController.aliasTopFiveCheapTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan').get(tourController.getMonthlyPlan);
+
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router
   .route('/id=:id')
   .get(tourController.getOneTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
 
 module.exports = router;

18deb69026a0e88c2769a837293ff89e364a9235
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-05-18 14:32:34 +0100

    feat: add gracefull shutdown & log errors

--- complete-node-bootcamp/4-natours/starter/server.js
+++ complete-node-bootcamp/4-natours/starter/server.js
@@ -1,8 +1,15 @@
+const http = require('http');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION');
+  process.exit(1);
+});
 
 const DB = process.env.DATABASE.replace(
   '<password>',
@@ -11,6 +18,16 @@ const DB = process.env.DATABASE.replace(
 
 mongoose.connect(DB).then(() => console.log('DB connection successful'));
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
+const host = process.env.HOST;
+const server = http
+  .createServer(app)
+  .listen(port, host, () => console.log(`App running on port ${host}:${port}`));
 
-app.listen(port);
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});

51bb908979e0c3ee0d7457c1a68ccc1e1255a249
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-05-18 14:21:25 +0100

    feat: add tour model

--- complete-node-bootcamp/4-natours/starter/models/tourModel.js
+++ complete-node-bootcamp/4-natours/starter/models/tourModel.js
@@ -1,58 +1,134 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+// const validator = require('validator');
 
-const tourSchema = new mongoose.Schema({
-  createdAt: {
-    type: Date,
-    default: Date.now(),
+const tourSchema = new mongoose.Schema(
+  {
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    images: [String],
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a imageCover'],
+    },
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      maxlength: [
+        40,
+        'A tour name must have have less than or equal 40 characters',
+      ],
+      minlength: [
+        10,
+        'A tour name must have have more than or equal 10 characters',
+      ],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        // only validates on new document creation
+        validator: function (val) {
+          return val < this.price;
+        },
+        // value is refering to the discount input request
+        message: 'Discount price ({value}) should be below regular price',
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    ratingsQuantitiy: {
+      type: Number,
+      default: 0,
+    },
+    secretTours: {
+      type: Boolean,
+      default: false,
+    },
+    startDates: [Date],
+    slug: String,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a decription'],
+    },
   },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  images: [String],
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a imageCover'],
-  },
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantitiy: {
-    type: Number,
-    default: 0,
-  },
-  startDates: [Date],
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a decription'],
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual('durationOfWeeks').get(function () {
+  return this.duration / 7;
 });
+// DOCUMENT MIDDLEWARE: runs bedore .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTours: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  console.log(docs);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTours: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+// // tourSchema.pre('save', (next) => {
+//   console.log('will save document');
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 

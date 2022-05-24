edd63a08df0cc9ba13bce0b71a7d5ea2b9cdfc41
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-05-24 16:30:39 +0100

    feat: create review model
    1. Reviews will be added to various tours.
    This will include rating & review linking the user & tour
    to the review.

---
+++ complete-node-bootcamp/4-natours/starter/models/reviewModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    review: {
+      type: String,
+      minlength: 5,
+      maxlength: 500,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      require: [true, 'Review must belong to a tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+    select: 'name photo',
+  });
+
+  next();
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;

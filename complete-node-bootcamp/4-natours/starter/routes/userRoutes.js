6cd222055955d55bb487be81102f514e11ff0c1c
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-05-24 16:33:07 +0100

    refactor: remove duplicate protect middleware

--- complete-node-bootcamp/4-natours/starter/routes/userRoutes.js
+++ complete-node-bootcamp/4-natours/starter/routes/userRoutes.js
@@ -6,17 +6,24 @@ const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgortPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+// Proetct all routes after authController middleware is called
+router.use(authController.protect);
 router.patch(
   '/updateMyPassword',
   authController.protect,
   authController.updatePassword
 );
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.get(
+  '/userProfile',
+  userController.getUserProfile,
+  userController.getUser
+);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
 router
   .route('/')

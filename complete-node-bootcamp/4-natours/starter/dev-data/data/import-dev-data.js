370d73f87ac81eb604eb68af90fbc37112566932
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-06-03 11:32:08 +0100

    refactor: log data updates

--- complete-node-bootcamp/4-natours/starter/dev-data/data/import-dev-data.js
+++ complete-node-bootcamp/4-natours/starter/dev-data/data/import-dev-data.js
@@ -31,6 +31,7 @@ const importData = async () => {
     await Tour.create(tours);
     await User.create(users, { validateBeforeSave: false });
     await Review.create(reviews);
+    console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
   }
@@ -43,6 +44,7 @@ const deleteData = async () => {
     await Tour.deleteMany();
     await User.deleteMany();
     await Review.deleteMany();
+    console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
   }

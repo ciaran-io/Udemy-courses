1a55243964a25212e84f8e6d27f803d7f80d0e68
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-06-07 17:22:07 +0100

    feat: upload new images for tours
    1. Use multer & sharp to handle image uploads
    for new tours or updating existing ones

--- complete-node-bootcamp/4-natours/starter/controllers/tourController.js
+++ complete-node-bootcamp/4-natours/starter/controllers/tourController.js
@@ -1,7 +1,61 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-const AppErrpor = require('../utils/appError');
+const AppError = require('../utils/appError');
+
+const multerStorage = multer.memoryStorage();
+
+// check image upload file  type
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload images only', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+  // cover image
+
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.webp`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1350)
+    .toFormat('avif')
+    .avif({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+  // images
+
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.webp`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('avif')
+        .avif({ quality: 75 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
+  next();
+});
 
 exports.aliasTopFiveCheapTours = (req, res, next) => {
   req.query.limit = '5';
